cc41cbb94bf5cbefdd9c90c4e43e0d8617023136
MD Waliul Islam Nohan <waliulnohan@gmail.com>
2023-07-21 03:03:22 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 import "./App.css";
 import Menu from "./components/menu"
+import Quiz from "./components/Quiz"
+import EndScreen from "./components/EndScreeen"
 import {GameStateContext} from './helpers/Contexts'
 
 function App() {
   const [gameState, setGameState] = useState("menu");
   const [userName,setUserName] = useState("");
+  const [score, setScore] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src="/logo.svg" className="App-logo" alt="logo" />
           <h1>QUIZ card</h1>
-          <GameStateContext.Provider value={{gameState, setGameState, userName,setUserName}}>
+          <GameStateContext.Provider value={{          gameState,
+          setGameState,
+          userName,
+          setUserName,
+          score,
+          setScore,}}>
               {gameState === 'menu' && <Menu/>}
+              {gameState === 'playing' && <Quiz/>}
+              {gameState === "finished" && <EndScreen />}
           </GameStateContext.Provider>
           
       </header>
